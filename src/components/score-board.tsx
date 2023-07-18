b30da58cd622f306fac2c7c13bd5b935fe1fecce
Brenth Miras <brenthmiras2@gmail.com>
2023-07-18 13:19:02 +0800

    Update score card styles

--- src/components/score-board.tsx
+++ src/components/score-board.tsx
@@ -9,19 +9,24 @@ type ScoreCardProps = {
 const ScoreCard = ({ score }: ScoreCardProps) => {
   return (
     <div className="card w-100 me-2 mb-2">
-      <div className="card-header">{score.name}</div>
+      {/* <div className="card-header">
+        <h2>{score.name}</h2>
+      </div> */}
       <div className="card-body">
-        <div>
-          <span className="badge bg-success me-2">{score.wins}</span>
-          Wins
-        </div>
-        <div>
-          <span className="badge bg-danger me-2">{score.loses}</span>
-          Losses
-        </div>
-        <div>
-          <span className="badge bg-secondary me-2">{score.draws}</span>
-          Draws
+        <h2 className="card-title">{score.name}</h2>
+        <div className="d-flex justify-content-between">
+          <div>
+            <span className="badge bg-success me-2">{score.wins}</span>
+            Wins
+          </div>
+          <div>
+            <span className="badge bg-danger me-2">{score.loses}</span>
+            Losses
+          </div>
+          <div>
+            <span className="badge bg-secondary me-2">{score.draws}</span>
+            Draws
+          </div>
         </div>
       </div>
     </div>
